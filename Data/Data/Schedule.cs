c237794ad07a77e53b4383680eeeb46e8939e23c
stoil.pla4kov@gmail.com <stoil.pla4kov@gmail.com>
2024-05-22 17:16:09 +0300

    cafene

--- Data/Data/Schedule.cs
+++ Data/Data/Schedule.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,15 @@ namespace Data.Data
 {
     public class Schedule : BaseEntity
     {
-
+        [Required]
+        public int TrainId { get; set; }
+        public virtual Train Train { get; set; }
+        [Required]
+        public int StationId { get; set; }
+        public virtual Station Station { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        [Required]
+        public DateTime DepartureTime { get; set; }
+        public int Platform { get; set; }
     }
 }
---
+++ Data/context/TrainsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.context
+{
+    public class TrainsDbContext : DbContext
+    {
+        public DbSet<Schedule> Schedules { get; set; }
+        public DbSet<Station> Stations { get; set; }
+        public DbSet<Train> Trains { get; set; }
+
+        public TrainsDbContext(DbContextOptions<TrainsDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
